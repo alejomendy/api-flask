93bdf206b786cff3cb196e098f1f34e539a5fd96
Diego <diego445.gonzalez@gmail.com>
2024-11-09 19:04:53 -0300

    Fix model usuario

--- app/models.py
+++ app/models.py
@@ -125,7 +125,7 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    rol = db.Column(db.String(50), nullable=False, default='User')
+    rol = db.Column(db.String(50), nullable=False, default='usuario')
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
